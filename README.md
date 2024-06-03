cc86619275536798441cc1289511158af0fbe65d
CAPO_AKS <91839305+AnjimaKS@users.noreply.github.com>
2024-06-03 14:30:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Django-Chat-app
-This is a simple django chat web application, you can chat with anyone by creating a room.
+# Dialogue (djangochat app)
+
+## user 1
+
+1. sign up with a username and password
+2. login to the chat app
+3. create a room
+4. sent messages
+5. log out
+
+## user 2
+
+1. sign up with a username and password
+2. login to the chat app
+3. enter into a room
+4. sent messages
+5. log out
---
+++ room/views.py
@@ -0,0 +1,27 @@
+# views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Room, Message
+from .forms import RoomForm
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+    return render(request, 'room/rooms.html', {'rooms': rooms})
+
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)[0:25]
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
+
+@login_required
+def create_room(request):
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rooms')
+    else:
+        form = RoomForm()
+    return render(request, 'room/create_room.html', {'form': form})
